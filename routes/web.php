f0da2412d7d972429de2f2eeaa5b57e75b71fc55
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-03-19 11:26:17 -0400

    si

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('login', function () {
+Route::get('/', function () {
     return redirect('admin/login');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Route::group(['prefix' => 'admin'], function () {
