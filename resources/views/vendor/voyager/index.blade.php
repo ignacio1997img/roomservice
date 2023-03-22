fd46fae7063c3e6090eb69dd4088d205b4fd8392
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-03-22 12:35:18 -0400

    si

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -15,22 +15,27 @@
                 @endphp
                 @foreach ($pdf as $item)
                     <div class="col-md-4" style="text-align: center">
-                        <div class="col-md-3"></div>
-                        <div class="col-md-6" style="background-color: #DFDFDF; margin-top: 2em; border-radius: 20px; height:320px">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8" id="myDiv" style="background-color: #DFDFDF; margin-top: 2em; border-radius: 20px; height:320px">
                             <br>
-                            <i class="fa-solid fa-house" style="color: #545454; font-size: 6em;"></i>
+                            <i class="fa-solid fa-house" style="color: #545454; font-size: 3em;"></i>
                             <p style="font-size: 22px; color: #ffffff;"><small>{{$item->name}}</small></p>                            
-                            <a href="{{route('view.planta', ['planta'=>$item->id])}}" class="btn btn-dark" data-toggle="modal">
+                            <a href="{{route('view.planta', ['planta'=>$item->id])}}" class="btn btn-primary" data-toggle="modal">
                                     <i class="fa-solid fa-eye"></i><span class="hidden-xs hidden-sm"> Ver</span>
                             </a>
-                            <br><br>
-                            <p style="text-align: left"><small>Habitaciones Libres:</small></p>
-                            <p style="text-align: left"><small>Habitaciones Ocupada:</small></p>
-                            <p style="text-align: left"><small>Total de Habitaciones:</small></p>
+                            <br><br><br><br>
+                            @php
+                                $libre =  \App\Models\Room::where('categoryFacility_id', $item->id)->where('status', 1)->where('deleted_at',null)->get();
+                                $ocupada =  \App\Models\Room::where('categoryFacility_id', $item->id)->where('status', 0)->where('deleted_at',null)->get();
+                                
+                            @endphp
+                            <p style="text-align: left"><small>Habitaciones Libres: <span style="font-size: 15px">{{count($libre)}}</span></small></p>
+                            <p style="text-align: left"><small>Habitaciones Ocupada: <span style="font-size: 15px">{{count($ocupada)}}</span></small></p>
+                            <p style="text-align: left"><small>Total de Habitaciones: <span style="font-size: 15px">{{count($libre)+count($ocupada)}}</span></small></p>
 
 
                         </div>   
-                        <div class="col-md-3"></div>
+                        <div class="col-md-2"></div>
 
                     </div>
                 @endforeach
@@ -42,6 +47,20 @@
             
         </div>
     </div>
+@stop
+@section('css')
+<style>
+    div#myDiv{
+        /* width:200px;
+        height:200px; */
+        background-image: url('https://fastrack-stamford.imgix.net/Stamford-Grand-Adelaide/Rooms/New-Suite-822-Bedroom.jpg?auto=%2Cformat%2Ccompress&fit=clip&ixlib=php-3.3.1&w=992');
+
+
+        background-repeat:no-repeat;
+        background-size:cover;
+        background-position:center center;  
+    }
+</style>
 @stop
 
 @section('javascript')
